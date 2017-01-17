03350f1e9d33a4bbd75e27a480868567d77479c6
GLOBAL\i076641 <jingjing.wang01@sap.com>
2017-01-17 12:47:07 +0800

    config

---
+++ zongmu_toolkit/Configs/CacheConfig.cs
@@ -0,0 +1,16 @@
+﻿using System.Configuration;
+
+namespace zongmu_toolkit.Configs
+{
+    public class CacheConfig : ConfigurationElement
+    {
+        [ConfigurationProperty("path", IsRequired = true)]
+        public string Dir
+        {
+            get
+            {
+                return this["path"] as string;
+            }
+        }
+    }
+}
---
+++ zongmu_toolkit/Configs/FfmpegConfig.cs
@@ -0,0 +1,26 @@
+﻿using System.Configuration;
+
+namespace zongmu_toolkit.Configs
+{
+    public class FfmpegConfig : ConfigurationSection
+    {
+        [ConfigurationProperty("path", IsRequired = true)]
+        public string Dir
+        {
+            get
+            {
+                return this["path"] as string;
+            }
+        }
+
+        public string FfProbe
+        {
+            get { return string.Format(@"{0}\ffprobe.exe", this.Dir); }
+        }
+
+        public string Ffmpeg
+        {
+            get { return string.Format(@"{0}\ffmpeg.exe", this.Dir); }
+        }
+    }
+}
